f2b4b20d53e0cbeb69937407bbb0ca78c8c63f1a
Flávio Domingos Gomes da Silva <161535510+flaviodgdas@users.noreply.github.com>
2025-02-26 10:26:52 -0300

    Update batalhaNaval-novato.c

--- batalhaNaval-novato.c
+++ batalhaNaval-novato.c
@@ -1,43 +1,39 @@
 #include <stdio.h>
-    // Batalha Naval
-    // criar tabuleiro tamanho 10x10
-    // onde (0) representa água e (1) representa partes do navio 1 (2)representa partes do navio 2
-    // um navio1 na horizontal e navio2 na vertical    
-    // void mostraTabuleiro() 
-    // void iniciaNaviosHorizontal() 
-    // void iniciaNaviosVertical()
-#define casas 10
-
-void iniciaNaviosHorizontal(int tabuleiro[casas][casas], int x, int y, int tamanho) {
-    for (int i = 0; i < tamanho; i++) {
-        tabuleiro[x][y + i] = 1; // Navio (1)
+void meuNavio1(int tabuleiro[10][10], int x, int y, int tamanho){
+    for (int i = 0; i < tamanho; i++){
+        tabuleiro[x][y + i] = 1;
     }
 }
-void iniciaNaviosVertical(int tabuleiro[casas][casas], int x, int y, int tamanho) {
-    for (int i = 0; i < tamanho; i++) {
-        tabuleiro[x + i][y] = 2; // Navio (2)
+void meuNavio2(int tabuleiro[10][10], int x, int y, int tamanho){
+    for (int i = 0; i < tamanho; i++){
+        tabuleiro[x + i][y] = 2;
     }
 }
-void mostraTabuleiro(int tabuleiro[casas][casas]) {
-    printf("*** BATALHA NAVAL - Novato ***\n");
+void meuTabuleiro(int tabuleiro[10][10]){
     printf("\n");
-    printf("  A B C D E F G H I J\n"); // Imprime letras das colunas
-    for (int i = 0; i < casas; i++) {
-        printf("%d ", i + 1); // Imprime números das linhas
-        for (int j = 0; j < casas; j++) {
+    printf("*** BATALHA NAVAL ***\n");
+    printf("       Novato\n");
+    printf("'0' representa agua\n");
+    printf("'1' representa Navio1\n");
+    printf("'2' representa Navio2\n");
+    printf("\n");
+    printf("  A B C D E F G H I J \n");
+    for (int i = 0; i < 10; i++){
+        printf("%d ", i + 1);
+        for (int j = 0; j < 10; j++){
             printf("%d ", tabuleiro[i][j]);
         }
-        printf("\n");        
-    }
+        printf("\n");   
+    }   
 }
-int main() {
-    int tabuleiro[casas][casas] = {0};
-    // Posicionar navio 1 (horizontal)tamanho 3 na posição (2,3)
-    iniciaNaviosHorizontal(tabuleiro, 2, 3, 3);
-    // Posicionar navio 2 (vertical)tamanho 3 na posição (5,6)
-    iniciaNaviosVertical(tabuleiro, 5, 6, 3);
-    // Mostrar o tabuleiro com os navios posicionados
-    mostraTabuleiro(tabuleiro);
-
+int main(){
+    int tabuleiro[10][10] = {0};
+    meuNavio1(tabuleiro, 2, 3, 3);
+    meuNavio2(tabuleiro, 5, 6, 3);
+    meuTabuleiro(tabuleiro);
+    printf("\n");
+    printf("*** Volte sempre! ***\n");
+    printf("\n");  
     return 0;
 }
+
